40e7adebf5da1344a7fd1e800d2970a46916e518
vakser <65089446+vakser@users.noreply.github.com>
2023-10-14 15:27:07 +0300

    Initial commit

---
+++ src/main/java/com/example/collectibles/BigStarCollectiblesApplication.java
@@ -0,0 +1,13 @@
+package com.example.collectibles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigStarCollectiblesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigStarCollectiblesApplication.class, args);
+	}
+
+}
