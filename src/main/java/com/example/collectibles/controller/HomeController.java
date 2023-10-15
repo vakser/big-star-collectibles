afa14ae4cc10485d2a4e22d1cecdec13255c3150
vakser <65089446+vakser@users.noreply.github.com>
2023-10-15 22:14:10 +0300

    HTML pages of binaryville collection linked from the homepage, home controller updated with method getCharacter, sql scripts added.

--- src/main/java/com/example/collectibles/controller/HomeController.java
+++ src/main/java/com/example/collectibles/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.example.collectibles.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
@@ -10,4 +11,10 @@ public class HomeController {
     public String displayHome(Model model) {
         return "index";
     }
+
+    @GetMapping("/getCharacter/{charname}")
+    public String getCharacter(@PathVariable("charname") String charName) {
+        return "/characters/" + charName;
+    }
+
 }
