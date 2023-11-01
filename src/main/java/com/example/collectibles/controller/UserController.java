5bac0c11610f565b0368389798120ee091476d70
vakser <65089446+vakser@users.noreply.github.com>
2023-11-01 16:26:55 +0200

    User bean created and validation constraints added.

--- src/main/java/com/example/collectibles/controller/UserController.java
+++ src/main/java/com/example/collectibles/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.example.collectibles.controller;
 
+import com.example.collectibles.beans.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
     @GetMapping("/newUser")
-    public String displayRegistrationForm() {
+    public String displayRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
         return "register-user";
     }
 }
