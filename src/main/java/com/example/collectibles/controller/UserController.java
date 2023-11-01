4ee035aac2c4ad01cd59949b1ea162aef58ae3e7
vakser <65089446+vakser@users.noreply.github.com>
2023-11-01 20:32:27 +0200

    Code updated to submit the new user registration form.

--- src/main/java/com/example/collectibles/controller/UserController.java
+++ src/main/java/com/example/collectibles/controller/UserController.java
@@ -1,15 +1,24 @@
 package com.example.collectibles.controller;
 
 import com.example.collectibles.beans.User;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
     @GetMapping("/newUser")
     public String displayRegistrationForm(Model model) {
         model.addAttribute("user", new User());
+        return "register-user";
+    }
+
+    @PostMapping("/saveUser")
+    public String saveUser(@ModelAttribute("user") @Valid User user, BindingResult result, Model model) {
         return "register-user";
     }
 }
