8d495630536c33e005c9581d231c1d7ee5aafca3
vakser <65089446+vakser@users.noreply.github.com>
2023-11-02 14:55:15 +0200

    InitBinder added to register a custom validator.

--- src/main/java/com/example/collectibles/controller/UserController.java
+++ src/main/java/com/example/collectibles/controller/UserController.java
@@ -1,16 +1,31 @@
 package com.example.collectibles.controller;
 
 import com.example.collectibles.beans.User;
+import com.example.collectibles.validators.UserValidator;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+    private UserValidator userValidator;
+
+    public UserController(UserValidator userValidator) {
+        this.userValidator = userValidator;
+    }
+
+    @InitBinder
+    public void bindUser(WebDataBinder binder) {
+        binder.addValidators(this.userValidator);
+    }
+
+
     @GetMapping("/newUser")
     public String displayRegistrationForm(Model model) {
         model.addAttribute("user", new User());
