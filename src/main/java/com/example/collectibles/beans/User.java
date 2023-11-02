22e40bff70cb6de6345d836b5e85ebb37047c25a
vakser <65089446+vakser@users.noreply.github.com>
2023-11-02 14:50:04 +0200

    Custom validator added to the app.

--- src/main/java/com/example/collectibles/beans/User.java
+++ src/main/java/com/example/collectibles/beans/User.java
@@ -11,21 +11,21 @@ import java.util.Date;
 public class User {
     @Id
     private Long id;
-    @NotEmpty(message = "Email cannot be empty")
-    @Email(message = "Should be a well-formed email address")
+//    @NotEmpty(message = "Email cannot be empty")
+//    @Email(message = "Should be a well-formed email address")
     private String email;
-    @NotEmpty(message = "Name cannot be empty")
-    @Size(min = 1, max = 20, message = "Size must be between 1 and 20")
+//    @NotEmpty(message = "Name cannot be empty")
+//    @Size(min = 1, max = 20, message = "Size must be between 1 and 20")
     private String name;
-    @NotEmpty(message = "Gender cannot be empty")
+//    @NotEmpty(message = "Gender cannot be empty")
     private String gender;
     private String message;
     private boolean newsLetter;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
-    @NotNull(message = "Date of birth cannot be empty")
-    @Past(message = "Date of birth cannot be a future date")
+//    @NotNull(message = "Date of birth cannot be empty")
+//    @Past(message = "Date of birth cannot be a future date")
     private Date dateOfBirth;
-    @NotEmpty(message = "Select at least one favorite character")
+//    @NotEmpty(message = "Select at least one favorite character")
     private String favoriteCollection;
 
     public Long getId() {
---
+++ src/main/java/com/example/collectibles/validators/UserValidator.java
@@ -0,0 +1,21 @@
+package com.example.collectibles.validators;
+
+import com.example.collectibles.beans.User;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+@Component
+public class UserValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return User.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        ValidationUtils.rejectIfEmpty(errors, "email", "email.empty");
+        ValidationUtils.rejectIfEmpty(errors, "name", "name.empty");
+    }
+}
