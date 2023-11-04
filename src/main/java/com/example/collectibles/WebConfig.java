4762dafd4e8c40be8526e87cf455f0fa06d67a20
vakser <65089446+vakser@users.noreply.github.com>
2023-11-04 15:50:23 +0200

    An interceptor class added and registered.

---
+++ src/main/java/com/example/collectibles/WebConfig.java
@@ -0,0 +1,14 @@
+package com.example.collectibles;
+
+import com.example.collectibles.interceptors.ApplicationInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new ApplicationInterceptor());
+    }
+}
